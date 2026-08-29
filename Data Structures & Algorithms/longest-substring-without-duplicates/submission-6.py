5d3e7e702c9b6ef4526102e56f2ef2227c029114
Grace Morgan <157081268+firefly313@users.noreply.github.com>
2026-08-28 21:42:58 -0400

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        current = 0
+        longest = 0
+        seen = {}
+        ## recurse forward
+        for let in s:
+            if let in seen:
+                seen = {}
+                current = 0
+                continue
+            else:
+                seen[let] = 1
+                current += 1
+                if current > longest:
+                    longest = current
+        ## recurse backwards
+        current = 0
+        seen = {}
+        for let in s[::-1]:
+            if let in seen:
+                current = 0
+                seen = {}
+                continue
+            else:
+                seen[let] = 1
+                current += 1
+                if current > longest:
+                    longest = current
+        return longest
