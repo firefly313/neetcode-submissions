69cb214ff203ea3acdf5238bea5ed092938bcce9
Grace Morgan <157081268+firefly313@users.noreply.github.com>
2026-09-11 17:47:43 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        ## prob better optimization to use set instead of dict
+        ## 1.) each row must contain the digits 1-9 without duplicates
+        ## loop through each row and make sure there is no duplicate
+        for row in range(len(board)):
+            check_row = {}
+            for col in range(len(board[row])):
+                curr = board[row][col]
+                ## check if its a number and in the dict
+                if curr.isnumeric() and curr in check_row:
+                    return False
+                else:
+                    check_row[curr] = 0
+                
+        ## 2.) each column must contain digits 1-9 without duplicates
+        for col in range(len(board[0])):
+            check_col = {}
+            for row in range(len(board)):
+                curr = board[row][col]
+                ## check if its a number and in the dict
+                if curr.isnumeric() and curr in check_col:
+                    return False
+                else:
+                    check_col[curr] = True
+
+        ## 3.) each of the 3x3 sub-boxes must contain the digits 1-9 no dupes
+        for row in range(0, 9, 3):
+            for col in range(0, 9, 3):
+                check_box = {}
+                for i in range(3):
+                    for j in range(3):
+                        curr = board[row+i][col+j]
+                        if curr.isnumeric() and curr in check_box:
+                            return False
+                        else:
+                            check_box[curr] = True
+
+        return True
