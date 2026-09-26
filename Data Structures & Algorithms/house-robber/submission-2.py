53e13a873d83c6c941544f5ad57323a2471c8633
Grace Morgan <157081268+firefly313@users.noreply.github.com>
2026-09-25 22:10:50 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        def dfs(i):
+            ## base cases
+            if i >= len(nums):
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+
+            rob = nums[i] + dfs(i+2)
+            skip = dfs(i+1)
+            dp[i] = max(rob, skip)
+            return dp[i]
+
+        return dfs(0)
